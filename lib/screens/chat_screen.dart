6f5c2d6314eb3eb2e146794709e48e0ca4c2738b
sarthakwe <sanuj.ps14@gmail.com>
2023-06-08 11:38:47 +0530

    first commit

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,7 @@
+import 'package:chat_app/theme.dart';
+import 'package:chat_app/widgets/glowing_action_button.dart';
+import 'package:chat_app/widgets/widgets.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../models/message_data.dart';
@@ -15,7 +19,310 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('data'),
+      appBar: AppBar(
+        iconTheme: Theme.of(context).iconTheme,
+        centerTitle: false,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leadingWidth: 54,
+        leading: Align(
+          alignment: Alignment.centerRight,
+          child: IconBackground(
+            icon: CupertinoIcons.back,
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
+        title: _AppBarTitle(messageData: messageData),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Center(
+              child: IconBorder(
+                icon: CupertinoIcons.video_camera_solid,
+                onTap: () {},
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: Center(
+              child: IconBorder(
+                icon: CupertinoIcons.phone_solid,
+                onTap: () {},
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: const [
+          Expanded(child: _DemoMessageList()),
+          _ActionBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class _DemoMessageList extends StatelessWidget {
+  const _DemoMessageList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: ListView(
+        children: const [
+          _DateLable(lable: 'Yesterday'),
+          _MessageTile(
+            message: 'Hi, Lucy! How\'s your day going?',
+            messageDate: '12:01 PM',
+          ),
+          _MessageOwnTile(
+            message: 'You know how it going...',
+            messageDate: '12:02 PM',
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _MessageTile extends StatelessWidget {
+  const _MessageTile({
+    super.key,
+    required this.message,
+    required this.messageDate,
+  });
+
+  final String message;
+  final String messageDate;
+
+  static const _borderRadius = 26.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(_borderRadius),
+                    topRight: Radius.circular(_borderRadius),
+                    bottomRight: Radius.circular(_borderRadius)),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12.0, vertical: 20),
+                child: Text(message),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                messageDate,
+                style: const TextStyle(
+                  color: AppColors.textFaded,
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _MessageOwnTile extends StatelessWidget {
+  const _MessageOwnTile({
+    super.key,
+    required this.message,
+    required this.messageDate,
+  });
+
+  final String message;
+  final String messageDate;
+
+  static const _borderRadius = 26.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                color: AppColors.secondary,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(_borderRadius),
+                    bottomLeft: Radius.circular(_borderRadius),
+                    bottomRight: Radius.circular(_borderRadius)),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12.0, vertical: 20),
+                child: Text(message),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                messageDate,
+                style: const TextStyle(
+                  color: AppColors.textFaded,
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _DateLable extends StatelessWidget {
+  const _DateLable({super.key, required this.lable});
+
+  final String lable;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 32.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 12),
+            child: Text(
+              lable,
+              style: const TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+                color: AppColors.textFaded,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _AppBarTitle extends StatelessWidget {
+  const _AppBarTitle({super.key, required this.messageData});
+
+  final MessageData messageData;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Avatar.small(
+          url: messageData.profilePicture,
+        ),
+        const SizedBox(
+          width: 16,
+        ),
+        Expanded(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              messageData.senderName,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontSize: 14,
+              ),
+            ),
+            const SizedBox(
+              height: 2,
+            ),
+            const Text(
+              'Online Now',
+              style: TextStyle(
+                fontSize: 10,
+                fontWeight: FontWeight.bold,
+                color: Colors.green,
+              ),
+            )
+          ],
+        ))
+      ],
+    );
+  }
+}
+
+class _ActionBar extends StatelessWidget {
+  const _ActionBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: true,
+      top: false,
+      child: Row(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              border: Border(
+                right: BorderSide(
+                  width: 2,
+                  color: Theme.of(context).dividerColor,
+                ),
+              ),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Icon(
+                CupertinoIcons.camera_fill,
+              ),
+            ),
+          ),
+          const Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(left: 16.0),
+              child: TextField(
+                style: TextStyle(fontSize: 14),
+                decoration: InputDecoration(
+                    hintText: 'Type something', border: InputBorder.none),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 12,
+              right: 24.0,
+            ),
+            child: GlowingActionButton(
+              color: AppColors.accent,
+              icon: Icons.send_rounded,
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
